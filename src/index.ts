731115ac547292c8c7ae2af32c3c77587e7be2a7
zeze.li <lizeze.boy@gmail.com>
2019-12-13 17:10:08 +0800

    add env

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
+import dotenv from "dotenv";
 import express from "express";
 import path from "path";
+
+// initialize configuration
+dotenv.config();
+
+// port is now available to the Node.js runtime
+// as if it were an environment variable
+const port = process.env.SERVER_PORT;
+
 const app = express();
-const port = 8080; // default port to listen
 
 // Configure Express to use EJS
-app.set( "views", path.join( __dirname, "views/" ) );
+app.set( "views", path.join( __dirname, "views" ) );
 app.set( "view engine", "ejs" );
 
 // define a route handler for the default home page
